7d6c3dad1ceafb8f29c1d1abdc5ecc5583428f44
bishen-gimhana <bishengimhana0@gmail.com>
2023-01-29 17:23:32 +0530

    order

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const authRoutes =require('./auth.route')
-;
+const authRoutes =require('./auth.route');
+const orderRoutes = require('./order.route')
 const router = express.Router();
 
 //localhost:4050/api/auth
 router.use('/auth',authRoutes);
+// http://localhost:4050/api/orders
+router.use("/orders", orderRoutes);
 
 
 module.exports=router;
